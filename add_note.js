4aaa035154773b90269d5f8f590b34d7487a2269
Aw3som3ss <jabwunza@gmail.com>
2023-11-02 16:07:22 +0300

    Add button functionality 2

--- add_note.js
+++ add_note.js
@@ -1,27 +1,27 @@
-// Define an array to store notes
-let notes = [];
+// Function to handle form submission and display added note
+function addNote(event) {
+    event.preventDefault(); // Prevent the form from submitting and page reloading
 
-// Function to add a new note
-function addNote() {
-	console.log('Button Clicked');
-	// Get user input for the note
-	const noteTitle = prompt('Enter the note title:');
-	const noteContent = prompt('Enter the note content:');
+    // Get user input for the note title and content
+    const noteTitle = document.getElementById("noteTitle").value;
+    const noteContent = document.getElementById("noteContent").value;
 
-	// Create a new note object
-	const newNote = {
-		title: noteTitle,
-		content: noteContent,
-	};
+    // Display the added note in a box
+    const noteDisplay = document.getElementById("noteDisplay");
+    const noteBox = document.createElement("div");
+    noteBox.className = "note-box";
+    noteBox.innerHTML = `<h2>${noteTitle}</h2><p>${noteContent}</p>`;
+    noteDisplay.appendChild(noteBox);
 
-	// Add the new note to the notes array
-	notes.push(newNote);
-
-	// Redirect to the "Add Note" page with the added note
-	window.location.href = `add_note.html?title=${encodeURIComponent(
-		noteTitle
-	)}&content=${encodeURIComponent(noteContent)}`;
+    // Optionally, you can reset the form fields after submission
+    document.getElementById("noteForm").reset();
 }
 
-const addNoteButton = document.getElementById('addNoteButton');
-addNoteButton.addEventListener('click', addNote);
+// Add a form submit event listener
+document.getElementById("noteForm").addEventListener("submit", addNote);
+
+// Show/hide the note container when the plus button is clicked
+document.getElementById("showNoteButton").addEventListener("click", function() {
+    const noteContainer = document.getElementById("noteContainer");
+    noteContainer.style.display = (noteContainer.style.display === "none") ? "block" : "none";
+});
